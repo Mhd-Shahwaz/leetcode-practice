1afc02b86dd8f2a07ae42f76c302c008b02e63a9
Shahwaz Khan <141860322+Mhd-Shahwaz@users.noreply.github.com>
2026-06-08 12:59:26 +0530

    Time: 25 ms (15.07%) | Memory: 153 MB (9.59%) - LeetSync

---
+++ 67-add-binary/add-binary.dart
@@ -0,0 +1,9 @@
+class Solution {
+    String addBinary(String a, String b) {
+    BigInt numA = BigInt.parse(a, radix: 2);
+    BigInt numB = BigInt.parse(b, radix: 2);
+
+    BigInt sum = numA + numB;
+    return sum.toRadixString(2); 
+  }
+}
