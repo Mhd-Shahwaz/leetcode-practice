d9f15dfd9fa595edba7d675931a475ae9449ecc3
Shahwaz Khan <141860322+Mhd-Shahwaz@users.noreply.github.com>
2026-06-17 23:33:48 +0530

    Time: 3 ms (81.02%) | Memory: 191.4 MB (37.23%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.dart
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.dart
@@ -2,7 +2,7 @@ class Solution {
 int maxProfit(List<int> prices) {
   int minPrice = prices.first;
   int maxProfit = 0;
-  for (int i = 1; i < prices.length; i++) {
+  for (int i = 0; i < prices.length; i++) {
     if (prices[i] < minPrice) {
       minPrice = prices[i];
     } else {
