e9dee76f6ea09b4b464646f2aaff4482707bde24
Shahwaz Khan <141860322+Mhd-Shahwaz@users.noreply.github.com>
2026-06-21 00:54:23 +0530

    Time: 1238 ms (5.66%) | Memory: 148.5 MB (88.68%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.dart
@@ -0,0 +1,17 @@
+class Solution {
+int removeDuplicates(List<int> nums) {
+  for (int i = 0; i < nums.length; i++) {
+    int count = 0;
+    for (int j = 0; j < nums.length; j++) {
+      if (nums[i] == nums[j]) {
+        count++;
+      }
+    }
+    if (count > 2) {
+      nums.removeAt(i);
+      i--;
+    }
+  }
+  return nums.length;
+}
+}
