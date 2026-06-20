2a4b014f4ce7e0f7e2292f7f1b0441ea0f7f1454
Shahwaz Khan <141860322+Mhd-Shahwaz@users.noreply.github.com>
2026-06-21 00:03:27 +0530

    Time: 0 ms (100.00%) | Memory: 149.3 MB (85.71%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.dart
+++ 74-search-a-2d-matrix/search-a-2d-matrix.dart
@@ -2,10 +2,8 @@ class Solution {
 bool searchMatrix(List<List<int>> matrix, int target) {
   bool isTargetFound = false;
   for (int i = 0; i < matrix.length; i++) {
-    for (int j = 0; j < matrix.first.length; j++) {
-      if (matrix[i].contains(target)) {
-        isTargetFound = true;
-      }
+    if (matrix[i].contains(target)) {
+      isTargetFound = true;
     }
   }
   return isTargetFound;
