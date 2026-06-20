68aa23dc49c6bf09b02998069ad368397870fe66
Shahwaz Khan <141860322+Mhd-Shahwaz@users.noreply.github.com>
2026-06-21 02:01:31 +0530

    Time: 175 ms (0.00%) | Memory: 182.8 MB (100.00%) - LeetSync

--- 164-maximum-gap/maximum-gap.dart
+++ 164-maximum-gap/maximum-gap.dart
@@ -1,7 +1,7 @@
 class Solution {
 int maximumGap(List<int> nums) {
   var maxGap = 0;
-  if (nums.length <= 1) return 0;
+  if (nums.length < 2) return 0;
   nums.sort();
   for (int i = 1; i < nums.length; i++) {
     int gap = nums[i] - nums[i - 1];
