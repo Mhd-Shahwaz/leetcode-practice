47d68032308951d9b3c447a76ce68e1ae4b40445
Shahwaz Khan <141860322+Mhd-Shahwaz@users.noreply.github.com>
2026-06-21 02:42:57 +0530

    Time: 2779 ms (8.33%) | Memory: 151.7 MB (83.33%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.dart
@@ -0,0 +1,20 @@
+class Solution {
+  List<int> majorityElement(List<int> nums) {
+    
+    int n=nums.length;
+    List<int>store=[];
+
+    for(int i=0;i<n;i++){
+        int count=0;
+        for(int j=0;j<n;j++){
+            if(nums[i]==nums[j]){
+                count++;
+            }
+        }
+        if(count>n/3 && !store.contains(nums[i])){
+          store.add(nums[i]);
+        }
+    }
+     return store;
+  }
+}
