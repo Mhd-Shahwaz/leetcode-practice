3d94fc01fd7e03660285cd7af10286447442a180
Shahwaz Khan <141860322+Mhd-Shahwaz@users.noreply.github.com>
2026-06-21 01:57:33 +0530

    Time: 156 ms (50.00%) | Memory: 189.5 MB (50.00%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.dart
@@ -0,0 +1,14 @@
+class Solution {
+int maximumGap(List<int> nums) {
+  var maxGap = 0;
+  if (nums.length <= 1) return 0;
+  nums.sort();
+  for (int i = 1; i < nums.length; i++) {
+    int gap = nums[i] - nums[i - 1];
+    if (gap > maxGap) {
+      maxGap = gap;
+    }
+  }
+  return maxGap;
+}
+}
