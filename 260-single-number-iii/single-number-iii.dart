b27ec9e14b36aad071caab8d890b88653218095a
Shahwaz Khan <141860322+Mhd-Shahwaz@users.noreply.github.com>
2026-06-22 11:29:12 +0530

    Time: 16 ms (0.00%) | Memory: 152.1 MB (0.00%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.dart
@@ -0,0 +1,14 @@
+class Solution {
+List<int> singleNumber(List<int> nums) {
+  nums.sort();
+  List<int> result = [];
+  for (int i = 0; i < nums.length; i++) {
+    if (result.contains(nums[i])) {
+      result.remove(nums[i]);
+    } else {
+      result.add(nums[i]);
+    }
+  }
+  return result;
+}
+}
