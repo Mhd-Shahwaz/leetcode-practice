8a2052340cb0d74de17ba31c1517a60f2fa1d461
Shahwaz Khan <141860322+Mhd-Shahwaz@users.noreply.github.com>
2026-06-22 11:57:30 +0530

    Time: 11 ms (20.00%) | Memory: 149.9 MB (50.00%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.dart
@@ -0,0 +1,17 @@
+class Solution {
+int singleNumber(List<int> nums) {
+  Map<int, int> freq = {};
+
+  for (int num in nums) {
+    freq[num] = (freq[num] ?? 0) + 1;
+  }
+
+  for (var entry in freq.entries) {
+    if (entry.value == 1) {
+      return entry.key;
+    }
+  }
+
+  return -1;
+}
+}
